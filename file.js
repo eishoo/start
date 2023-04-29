f2c12ef55c7f0b44e8286246a2ea1eefeaafda5e
eishoo <eishalmalik858@gmailcom>
2023-04-29 01:04:43 -0700

    first commit

---
+++ file.js
@@ -0,0 +1,159 @@
+// ways to print js
+// console.log("hello world")
+// // document.write("this is document write")
+// console.warn("this is warning")
+// console.error("this is error")
+// console.api
+console.log("hello world", 4 + 6, "another log")
+// document.write("this is document write")
+console.warn("this is warning")
+console.error("this is error")
+// javascript variables 
+// what are containers? containers to store data values
+var number1 = 34;
+var number2 = 56;
+console.log(number1+number2);
+
+// 4. data types in javascript 
+// numbers
+var num1 = 455;
+var num2 = 56.76
+// string
+var str1 = "this is a string";
+var str2 = "this is also a string";
+// objects
+var marks = {
+    a: 34,
+    b: 78,
+    c:99.977
+}
+console.log(marks)
+console.log(str1)
+// booleans
+var a = true;
+var b = false;
+console.log(a,b);
+
+// var und = undefined
+var und = 
+console.log(undefined);
+var n = null;
+console.log(n);
+/*
+at a high level, there are two types of data types in javascript 
+1. primitive datatypes:undefined,null,num,string,boolean,symbol
+2. refrence datatype:arrays and objects
+*/
+var arr =  [ 1, 2, "string" ,4 ]
+console.log (arr)
+
+// operators in javascvript 
+var a = 100;
+var b =110;
+console.log("the vale of a+b",a+b);
+console.log("the vale of a-b",a-b);
+console.log("the vale of a*b",a*b);
+console.log("the vale of a/b",a/b);
+
+// assingment operators 
+var c = b;
+// c += 2;
+// c -= 2; // c = c -2
+// c *= 2;
+// c /= 2;
+// console.log(c);
+// comparision operators
+var x = 34;
+var y = 56;
+// console.log ( x == y)
+// console.log ( x <= y)
+// console.log ( x >= y)
+// console.log ( x > y)
+// console.log ( x < y)
+
+// logical operators
+// logical and
+// console.log (true && true)
+// console.log (true && false)
+// console.log (false && false)
+// console.log (false && true)
+// logical or
+// console.log (true || true)
+// console.log (true || false)
+// console.log (false || false)
+// console.log (false || true)
+// logical not
+// console.log(!false);
+// console.log(!true);
+
+// function in javascript
+// dry= do not reapeat yourself
+function avg (a,b) {
+    return (a+b)/2;
+}
+c1 = avg(4,6);
+c2 = avg(14,16);
+// console.log(c1,c2);
+
+// condtionals in javascript
+var age = 4
+// single if statement
+// if (age > 18) {
+    // console.log('you can drink water rasna kid') 
+// }
+// if - else statement
+// else{
+    // console.log('you cannot drink rasna water kid')
+// }
+
+// if else ladder
+// if (age>32) {
+    // console.log("you are not a kid")
+// }
+// else if(age >26){
+    // console.log("bachy ni ho")
+// }
+
+// for loop
+
+var arr =[1,2,3,4,5]
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+    // console.log(arr[i])
+// }
+// arr.forEach(function(element) {
+    // console.log(element);
+// })
+
+let j=0
+
+   
+
+// var 1 
+
+
+// var
+// let
+// const
+
+// var first_name = "hello world";
+// document.write(first_name);
+// document.write(y);
+
+let first_name = "alpha";
+document.write(first_name)
+
+// const
+
+const second ="hello";
+
+document.write(second);
+
+
+
+
+
+
+
+
+
